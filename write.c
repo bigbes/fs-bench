ee086c3c54e5930ab17e2a4a19ad43024b5ef55d
bigbes <bigbes@debian.(none)>
2012-04-09 18:49:42 +0400

    bugfixes in *.c

--- write.c
+++ write.c
@@ -8,7 +8,7 @@ int main(){
     int fd = open("./tempfc", O_RDWR | O_CREAT);
     ssize_t ret;
     char c[] = "12345\n";
-    while ((ret = write(fd, c, sizeof(c))) != -1);
+    while ((ret = write(fd, c, sizeof(c))) == sizeof(c));
     int sig = fcntl(fd, 11);
     printf("%d %d %d\n", ret, sig, errno);
     close(fd);
